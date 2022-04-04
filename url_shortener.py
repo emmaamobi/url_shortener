f7fc65dbf6c2c647e1a7a5befbd1544416c757ef
Emmanuel Amobi <amobisomto@gmail.com>
2022-04-04 15:58:17 -0500

    encode and decode endpoints

--- url_shortener.py
+++ url_shortener.py
@@ -2,86 +2,105 @@ from flask import Flask, jsonify, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import os
 import string
-import sqlite3
+from datetime import datetime
+from random import choices
 
 # setup configs
 app = Flask(__name__)
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-
-ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME')
-ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
 
 
-db = SQLAlchemy(app)
-
-# class Url(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     original_url = db.Column(db.String(550))
-#     shortened_url = db.Column(db.String(10), unique=True)
-#     time_created = db.Column(db.DateTime, default=datetime.now)
+urls = {} # url: [original, short, created_at]
+short_suffixes = set()
+short_urls = {}
+shortened_urls = {}
 
 @app.route("/")
 def home_page():
     return "<p>Url shortener</p>"
 
-# todo: fix this 
 @app.route('/encode',methods=['POST'])
 def encode_url():
 
     # ensure uniqueness
-    def generate_short(cursor):
+    def generate_short(suffixes):
         short = ''.join(choices(chars,k=4))
-        exist = cursor.execute("select short_url from urls where short_url = {}".format(short), (short_url,)).fetchone()
-        if exist:
-            generate_short(cursor)
+        if short in suffixes:
+            generate_short(suffixes)
         else:
-            return short
+            return short 
 
     url = str(request.json['url'])
-    print("URL+++++++++++++++++++")
-    print("URL IS: ", url)
-    sqlconnection = sqlite3.connect("database.db")
-    cursor = sqlconnection.cursor()
     chars = string.digits + string.ascii_letters
     # check if url exists already
-    exist = cursor.execute("SELECT * from URLS where original_url={}".format(url))
-    print("EXISTS: ",  exist)
 
-    if exist:
-        short = exist
+    if url in urls:
+        short = urls[url][1]
     else:
-        short = generate_short(cursor)
-        cursor.execute("INSERT INTO urls(original_url,short_url) VALUES ('{}', '{}')".format(url, short))
-        sqlconnection.commit()
+        short = generate_short(short_suffixes)
+        short_suffixes.add(short)
+        urls[url] = []
+        urls[url].append(url)
+        urls[url].append(short)
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        urls[url].append(current_time)
 
 
-    created_at = cursor.execute("SELECT created from urls where short_url={}".format(short)).fetchone()
-    sqlconnection.close()
 
-    shortened_url = request.host_url + short
+    created_at = urls[url][2]
+
+    shortened_url = request.host_url + "short?url=" + short
+    shortened_urls[shortened_url] = url
+    short_urls[short] = url
+    print("SHORTTT")
+    print(short_urls)
     data = {
         'url': url,
         'shortened': shortened_url,
         'date created': created_at
     }
     return jsonify(data)
-# @app.route('decode', )
+
 @app.route('/decode',methods=['POST'])
 def decode_url():
-    url = request.json['url']
-    url = request.host_url + url
+    url = str(request.json['url'])
+    print("URL : ", url)
+    if url not in shortened_urls:
+        return jsonify({"error": "shortened url does not exist"})
+    decoded = shortened_urls[url]
+    print("short_urls: ", short_urls)
+    time_stamp = urls[decoded][2]
     data = {
-        'url': url,
-        'shortened': 'blah blah',
-        'date created': 'blah blah'
+        'url': decoded,
+        'shortened': url,
+        'date created': time_stamp
     }
     return jsonify(data)
+
 @app.route('/short/')
+def open_url():
+    short_url = request.args.get('url', default = 1, type = str)
+    if short_url not in short_urls:
+        return '',404
+    redirect_url = short_urls[short_url]
+    print("REDIRECT URL: ", redirect_url)
+    return redirect(redirect_url)
 
 
-@app.route('/stored_urls', methods=['GET'])
+@app.route('/stored', methods=['GET'])
 def list_all():
-    urls = []
-    urls.append({'url': 'ssdd'})
-    return jsonify(urls)
+    cur_urls = []
+    for url in urls:
+        original = urls[url][0]
+        short = urls[url][1]
+        short = request.host_url + "short?url=" + short
+        time_ = urls[url][2]
+        append_ = {
+            'url': original,
+            'shortened': short,
+            'date created': time_
+        }
+        cur_urls.append(append_)
+
+    return jsonify(cur_urls)
 
